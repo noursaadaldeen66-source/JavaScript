39d21fdc559e17fb1448d91ae78373f1134a75ae
noursaadaldeen66-source <nour.saadaldeen66@gmail.com>
2025-08-27 12:24:59 +0300

    summing Function

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,45 @@
-let x =13;
-let y =11;
-let z =12;
+// let x =13;
+// let y =11;
+// let z =12;
 
-max=0;
+// max=0;
 
-if(x > y){
-    max =x;
-}else max =y;
-if (z > max){
-    max= z;
-}
-console.log(max);
+// if(x > y){
+//     max =x;
+// }else max =y;
+// if (z > max){
+//     max= z;
+// }
+// console.log(max);
+
+
+
+
+function summing(num){
+    if (num < 0){
+        return 0
+    }else{
+        let i = 1;
+        let sum =0;
+    while(i <= num){
+       sum = sum + i;
+       i++;
+        
+    
+        
+    }
+     return sum
+    }
+    
+} 
+
+console.log(summing(7));
+
+
+
+
+
+
+
+
+
